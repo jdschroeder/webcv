3f343a1556ae5a59d5b4c8904a328176d4dc5a58
Joshua Schroeder <schrojo@gmail.com>
2012-05-22 17:01:49 -0600

    Adding a basic framework for a web CV

---
+++ scripts/webcv.js
@@ -0,0 +1,11 @@
+$("document").ready(function() {
+
+  // Add the HTML entity codes for stars on the skill ratings
+  $(".1star").prepend("&#9733;&#9734;&#9734;&#9734;&#9734; ");
+  $(".2star").prepend("&#9733;&#9733;&#9734;&#9734;&#9734; ");
+  $(".3star").prepend("&#9733;&#9733;&#9733;&#9734;&#9734; ");
+  $(".4star").prepend("&#9733;&#9733;&#9733;&#9733;&#9734; ");
+  $(".5star").prepend("&#9733;&#9733;&#9733;&#9733;&#9733; ");
+  
+  
+});
